af2fcc81797e6bfb5b5e2267bed09ccaa5349adc
artur0513 <artur.salaridze@yandex.ru>
2022-09-18 21:10:44 +0300

    fix

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -35,7 +35,7 @@ bool test(int size, bool print = true) {
 
 	srand(time(0));
 	for (int i = 0; i < size; i++) {
-		k[i] = rand() % 100;
+		k[i] = rand() % 10000;
 		if (print)
 			cout << k[i] << " ";
 	}
