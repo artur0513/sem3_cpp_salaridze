7f780f6aae90ff9e37e0dc3126e190be12101646
artur0513 <artur.salaridze@yandex.ru>
2022-09-18 20:30:49 +0300

    18.09.22

---
+++ priority_order/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <deque>
+#include <utility>
+
+using namespace std;
+
+template<class T> class Queue {
+public:
+	deque<pair<T, int>> items;
+
+	void add(T new_item, int prior) {
+		for (int i = 0; i < items.size(); i++) {
+			if (prior > items[i].second) {
+				items.insert(items.begin() + i, pair<T, int>(new_item, prior));
+				return;
+			}
+		}
+		items.push_back(pair<T, int>(new_item, prior));
+	}
+
+	void pop_back() {
+		if (items.size() > 0) {
+			items.pop_front();
+		}
+	}
+
+	void print() {
+		for (int i = 0; i < items.size(); i++) {
+			cout << items[i].first << " " << items[i].second << endl;
+		}
+		cout << endl;
+	}
+};
+
+int main() {
+	Queue<string> q;
+	q.add("aaa", 10);
+	q.add("bbb", 15);
+	q.add("ccc", 25);
+	q.add("ddd", 20);
+	q.add("sodfn", 3);
+	q.print();
+	q.pop_back();
+	q.print();
+}
---
+++ quicksort/quicksort.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<class T> size_t partition(T* data, size_t r) {
+	size_t l = 0;
+	size_t support_element = (data[l] + data[r]) / 2;
+
+	while (true) {
+		while (data[l] < support_element)
+			l++;
+		while (data[r] > support_element)
+			r--;
+		if (r <= l)
+			return r;
+
+		swap(data[l++], data[r--]);
+	}
+}
+
+template<class T> void quicksort(T* data, size_t size) {
+	if (size < 1)
+		return;
+	size_t split = partition(data, size);
+	quicksort(data, split);
+	quicksort(data + split + 1, size - split - 1);
+}
+
+bool test(int size, bool print = true) {
+	// if print == true to pechataem v consol massiv
+	int* k = new int[size];
+
+	if (print)
+		cout << "test na massive iz " << size << " elementov" << endl;
+
+	srand(time(0));
+	for (int i = 0; i < size; i++) {
+		k[i] = rand();
+		if (print)
+			cout << k[i] << " ";
+	}
+	if (print)
+		cout << endl;
+
+	quicksort(k, size - 1);
+
+	bool correct_sort = true;
+	for (int i = 0; i < size; i++) {
+		if (i != 0) {
+			if (k[i] < k[i - 1])
+				correct_sort = false;
+		}
+		if (print)
+			cout << k[i] << " ";
+	}
+	if (print)
+		cout << endl;
+	delete[] k;
+	return correct_sort;
+}
+
+int main() {
+	if (test(20))
+		cout << "correct" << endl;
+	else
+		cout << "wrong" << endl;
+
+	cout << endl << "test na 10000 elementov (1 = correct, 0 = wrong): " << test(10000, false) << endl;
+}
